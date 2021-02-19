d8916ec02ad36347d8cbb9cf8a6ebf693a4c2b96
G3rm4nS4nch3z <64222516+G3rm4nS4nch3z@users.noreply.github.com>
2021-02-19 14:48:30 +0100

    Delete flota_kanban directory

--- flota_kanban/models/flota_kanban.py
+++
@@ -1,8 +0,0 @@
-#-*- coding: utf-8 -*-
-
-from odoo import models, fields
-
-class TodoTask(models.Model):
-    _inherit = 'crear.flota'
-    priority = fields.Selection([('0','Low'),('1','Normal'),('2','High')],'Priority',default='1')
-    kanban_state = fields.Selection([('normal', 'En camino'),('blocked', 'Bloqueado'), ('done', 'Listo para la siguiente entrega')],'Estado de Entregas',default='normal')
