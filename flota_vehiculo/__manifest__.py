a3af45608c53369cfb7676e478c59d67f0add6c4
G3rm4nS4nch3z <64222516+G3rm4nS4nch3z@users.noreply.github.com>
2021-02-19 14:48:49 +0100

    Delete flota_vehiculo directory

--- flota_vehiculo/__manifest__.py
+++
@@ -1,8 +0,0 @@
-{
-	'name': 'Vehiculos de flota',
-	'version': '1.0',
- 	'author': 'Domo Sapien',
- 	'description': """ Espicificacion de las caracteristicas de los vehiculos""",
-	'depends': ['flota_app','mail'],
-	'data': ['views/flota_vehiculo_views.xml'],
-}
