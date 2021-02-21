454ca49cbf5dbdfcbcd588d8cfb415d76f0d3e57
G3rm4nS4nch3z <64222516+G3rm4nS4nch3z@users.noreply.github.com>
2021-02-21 20:22:23 +0100

    Add files via upload

--- flota_app/flota_app.py
+++ flota_app/flota_app.py
@@ -16,7 +16,7 @@ class CrearFlota(models.Model):
 
 class DevSet(models.Model):
    _name = 'crear.flota.set'
-   set = fields.Many2one('personalizacion.set', required=True)
-   cantidad = fields.Integer('Cantidad del Set', required=True)
+   set = fields.Many2one('personalizacion.set')
+   cantidad = fields.Integer('Cantidad del Set')
    set_id = fields.Many2one('crear.flota', 'Sets', ondelete='cascade')
      
